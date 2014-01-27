7383e5bfb300401e0e005084bfed1bd15220cbee
zekezang <zeke_zang@cn.shuttle.com>
2014-01-27 14:07:42 +0800

    init by zekezang

---
+++ include/config.h
@@ -0,0 +1,5 @@
+/* Automatically generated - do not edit */
+#define CONFIG_BOARDDIR board/samsung/micro2440
+#include <config_defaults.h>
+#include <configs/micro2440.h>
+#include <asm/config.h>
---
+++ include/config.mk
@@ -0,0 +1,5 @@
+ARCH   = arm
+CPU    = arm920t
+BOARD  = micro2440
+VENDOR = samsung
+SOC    = s3c24x0
